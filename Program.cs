286f867d0d2dc44389eadc9d1959e4878d7f2cde
M4KS1m29 <ostrovskyi_ak24@nuwm.edu.ua>
2024-12-04 23:05:43 +0200

    Додав змни Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,53 @@
-﻿using System;
+﻿
+using System;
+using System.Text.RegularExpressions;
 
-namespace LabWork
+class Program
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
+        string text = "Інформатика - це наука про інформацію. Інформатика включає різні розділи. Програмування - це частина інформатики.";
+
+        // Регулярний вираз для пошуку речень, що починаються зі слова "Інформатика"
+        string pattern = @"\bІнформатика\b";
+
+        // Знаходимо всі збіги
+        MatchCollection matches = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
+
+        // Виводимо кількість знайдених речень
+        Console.WriteLine("Кількість речень, які починаються зі слова 'Інформатика': " + matches.Count);
     }
 }
+
+//Додаткове завдання!!
+
+
+//using System;
+//using System.Text.RegularExpressions;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        string text = "вк 0964 вс." +
+//            "аа 0924 нк." +
+//            "вн 3542 то." +
+//            "нн 7891 цу." +
+//            "ск 5435 фк." +
+//            "но 9003 ва." +
+//            "вк 4098 он." +
+//            "ак 4060 пк." +
+//            "ао 4060 пк." +
+//            "вм 8932 фу.";
+
+//        string pattern = @"\bвк \d{4} [а-я]{2}\b";
+
+//        MatchCollection matches = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
+
+//        Console.WriteLine("Номерні знаки, що починаються на 'вк':");
+//        foreach (Match match in matches)
+//        {
+//            Console.WriteLine(match.Value);
+//        }
+//    }
+//}
